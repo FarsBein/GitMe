d1e579c7b86d0c6029fdfbd8fb68a4807ddfc800
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-05-08 13:45:50 -0400

    Connect to MongoDB Atlas cluster with Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose')
+let count = 0; //just for my own sanity (not need it)
+require('dotenv').config() 
 
 const app = express();
-
 const PORT = process.env.PORT || 8000;
+
+//connect to mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true // to avoid annoying pop ups asking for upgrade
+}, (err) => {
+    if (err) throw err
+    console.log('MongoDB connection is established')
+    count++; //(not need it)
+})
 
 app.get('/', (req,res)=> {
     res.status(200).send('WE ARE WORKING FROM HOME!')
