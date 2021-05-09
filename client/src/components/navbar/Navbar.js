4405067010206a1e31ca4904c480d4c67412eb85
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-05-09 15:10:49 -0400

    made font bigger and spread titles on the Navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,20 +1,80 @@
 import './Navbar.css';
 import {Link} from 'react-router-dom'; 
 import * as ReactBootStrap from "react-bootstrap"
+import {AiFillGithub} from 'react-icons/ai';
+
 
 function Navbar() {
   return (
-        <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="navbar">
-            <ReactBootStrap.Navbar.Brand href="#home">React-Bootstrap</ReactBootStrap.Navbar.Brand>
-            <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav" className="">
-                <ReactBootStrap.Nav className="mr-auto">
-                    <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
-                    <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-            </ReactBootStrap.Nav>
-            </ReactBootStrap.Navbar.Collapse>
+        <ReactBootStrap.Navbar collapseOnSelect expand="md" bg="dark" variant="dark" className="navbar">
+            <ReactBootStrap.Container >
+                <ReactBootStrap.Navbar.Brand href="#home" className="logo-font">GitMe</ReactBootStrap.Navbar.Brand>
+                <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav" className="space-between">
+                    <ReactBootStrap.Nav className="mr-auto">
+                        <ReactBootStrap.Nav.Link href="#features">Home</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href="#pricing">Doc</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href="#pricing">Showcase</ReactBootStrap.Nav.Link>
+                    </ReactBootStrap.Nav>
+                    <ReactBootStrap.Nav className='justify-content-center align-content-lg-center'>
+                        <ReactBootStrap.Nav.Link href="#deets">About us</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link eventKey={2} href="#memes">Source Code</ReactBootStrap.Nav.Link>
+                    </ReactBootStrap.Nav>
+                </ReactBootStrap.Navbar.Collapse>
+            </ReactBootStrap.Container>
         </ReactBootStrap.Navbar>
   );
 }
 
 export default Navbar;
+
+// import * as ReactBootStrap from "react-bootstrap"
+// import React, {useState} from 'react'
+// import './Navbar.css';
+// import {Link} from 'react-router-dom'; 
+// import {FaBars, FaTimes, AiFillGithub} from 'react-icons/fa'
+
+// function Navbar() {
+//     const [click,setClick] = useState(false)
+
+//     const menuClickHandler = () => {
+//         setClick(!click)
+//     }
+
+//     return (
+//         <div className='navbar'>
+//             <div className="navbar-container container">
+//                 <Link to='/' className='logo'>
+//                     GITME
+//                 </Link>
+//                 <div className="menu-icon" onClick={menuClickHandler}>
+//                     {click ? <FaBars/> : <FaTimes/>}
+//                 </div>
+//                 <ul className={click ? 'nav-menu active': 'nav-menu'}>
+//                     <li className='nav-item'>
+//                         <Link to='/' className="nav-item link">
+//                             Home
+//                         </Link>
+//                     </li>
+//                     <li className='nav-item'>
+//                         <Link to='/' className="nav-item link">
+//                             Contact
+//                         </Link>
+//                     </li>
+//                     <li className='nav-item'>
+//                         <Link to='/' className="nav-item link">
+//                             About us
+//                         </Link>
+//                     </li>
+//                     <li className='nav-item'>
+//                         <Link to='/' className="nav-item link">
+//                             Source Code
+//                         </Link>
+//                     </li>
+//                 </ul>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Navbar;
