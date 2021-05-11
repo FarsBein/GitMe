c8471a3ebdef8f57faa2952ad729d04897c09f42
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-05-11 15:52:32 -0400

    remove images from dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,39 @@
 import './Dashboard.css';
 import '../SharedStyle.css';
+import background2 from '../../images/background2.svg'
+import edit2 from '../../images/edit2.svg'
+import logout2 from '../../images/logout2.svg'
+import { Link } from 'react-router-dom';
 
 function Dashboard() {
   return (
-    <div className="jumbo">
-
-        <p>
-            bruh it is simple just sign in and you are good               
-        </p>
-        <p>
-        </p>
+    <div className='container' >
+      <div className="jumbo-dashboard">
+        <Link to='/create-website'>
+          <div className="cards">
+              {/* <img src={edit2} className='dashboard-cards-icons'/> */}
+              <div>
+                <h4><b>Edit website</b></h4> 
+              </div>
+          </div>
+        </Link>
+        <Link to='/'>
+          <div className="cards">
+              {/* <img src={background2} className='dashboard-cards-icons'/> */}
+              <div>
+                <h4><b>Change projects background</b></h4> 
+              </div>
+          </div> 
+        </Link>
+        <Link to='/'>
+          <div className="cards">
+              {/* <img src={logout2} className='dashboard-cards-icons'/> */}
+              <div>
+                <h4><b>Logout</b></h4> 
+              </div>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
