fa16e320621fc0aa101bc3364de009080007adca
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-05-17 21:05:10 -0400

    moved auth to router folder

---
+++ routes/auth-routes.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+
+// check if user is logged in 
+const isLoggedin = function (req,res,next) {
+    if (req.user) {
+        next()
+    } else {
+        res.redirect('/login');
+    }
+}  
+
+// steps to take when authenticating user
+
+// 1 make the call to github 
+router.get('/github', passport.authenticate('github')) 
+
+// 2 receive and handle the response
+router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/auth/login' }), //was "/auth/error"
+  (req, res) => {
+    res.redirect('/auth/dashboard'); //was "/auth/pass"
+});
+
+// 3 handle error in connection
+router.get('/error', (req, res) => res.send('Unknown Error'))
+
+// 4 handle successful connection
+router.get('/pass', (req, res) => res.send('SUCCESSFUL'))
+
+//for testing <<<
+router.get('/dashboard', isLoggedin, (req,res)=> {
+    console.log(req.user)
+    res.sendFile(__dirname + '/dashboard.html');
+})
+
+router.get('/login', (req,res)=> {
+    if (req.user) {
+        res.redirect('/auth/dashboard');
+    } else {
+        res.sendFile(__dirname + '/login.html');
+    }
+})
+
+router.get('/logout', isLoggedin, (req, res) => {
+    req.logOut();
+    res.redirect('/auth/login');
+});
+//for testing >>>
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors')
+const authRoutes = require("./routes/auth-routes")
+
+// passport 
+const GitHubStrategy = require('passport-github').Strategy;
+const passport = require('passport')
+const session = require('express-session')
+
 require('dotenv').config() 
-var cors = require('cors')
+
 
 //set up ports 
 const app = express();
@@ -24,96 +32,59 @@ mongoose.connect(process.env.MONGODB_URI, {
 })
 
 //Github OAuth (checked out https://www.npmjs.com/package/passport-github)
-const GitHubStrategy = require('passport-github').Strategy;
-const passport = require('passport')
-const session = require('express-session')
-const Auth = function (req,res,next) {
-    if (req.user) {
-        next()
-    } else {
-        res.redirect('/login');
-    }
-}  
+
+
 
 app.use(session({
     secret: process.env.SECRET_SESSION_KEY,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        httpOnly:true, // don't save on browser only on server
-        secure: false, // only bc I am testing it on my local env change to true latter
-        maxAge: 2 * 24 * 60 * 60 * 1000
+        httpOnly:true,                  // means to not save session on browser only on server
+        secure: false,                  // only bc I am testing it on my local env change to true latter
+        maxAge: 2 * 24 * 60 * 60 * 1000 // two day (first number is days)
     }
 }))
 
 app.use(passport.initialize())
 app.use(passport.session())
 
-passport.serializeUser(function (user, cb) {
-    cb(null, user.id)
+passport.serializeUser(function (user, done) {
+    done(null, user.id) // id from my database can be found as "_id"
 })
-passport.deserializeUser(function (id, cb) {
-    cb(null, id)
+passport.deserializeUser(function (id, done) {
+    // check if the id is a real user in my database. If yes retrieve user data
+    // User.findById((id)).then((user) => {
+    //     done(null, user)
+    // }) //it is async and returns a promise so it will wait 
+    done(null, id)
 })
 
-passport.use(new GitHubStrategy({
+passport.use(
+    new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: "http://localhost:8000/auth/github/callback"
   },
-  function(accessToken, refreshToken, profile, cb) {
+  function(accessToken, refreshToken, profile, done) {
     // for mongodb 
     // User.findOrCreate({ githubId: profile.id }, 
     //     function (err, user) {
-    //         return cb(err, user);
+    //         return done(err, user);
     //     }
     // );
     // console.log('profile:',profile);
-    return cb(null, profile);
+    return done(null, profile);
   }
 ));
 
-// steps to take when authenticating user
 
-// 1 make the call to github 
-app.get('/auth/github', passport.authenticate('github')) 
-
-// 2 receive and handle the response
-app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/auth/error' }), //was "/login"
-  (req, res) => {
-    res.redirect('/auth/pass'); //was "/dashboard"
-});
-
-// 3 handle error in connection
-app.get('/auth/error', (req, res) => res.send('Unknown Error'))
-
-// 4 handle successful connection
-app.get('/auth/pass', (req, res) => res.send('SUCCESSFUL'))
-
-// route 
+// routes 
 app.get('/', (req,res)=> {
     res.status(200).send('WE ARE WORKING FROM HOME!')
 })
 
-//for testing <<<
-app.get('/dashboard', Auth, (req,res)=> {
-    console.log(req.user)
-    res.sendFile(__dirname + '/dashboard.html');
-})
-
-app.get('/login', (req,res)=> {
-    if (req.user) {
-        res.redirect('/dashboard');
-    } else {
-        res.sendFile(__dirname + '/login.html');
-    }
-})
-
-app.get('/logout', Auth, (req, res) => {
-    req.logOut();
-    res.redirect('/login');
-});
-//for testing >>>
+app.use('/auth', authRoutes)
 
 // listener 
 app.listen(PORT, console.log(`Server is starting at port ${PORT}`));
